6944205538708dc3bd2c5f8b10f01fe702d08e11
sharispillai <sharispillai2005@gmail.com>
2026-06-21 22:34:19 +0530

    Add files via upload

--- Calculator without using function.py
+++ Calculator without using function.py
@@ -2,7 +2,7 @@
 print("welcome to the calculator")
 x=int(input("enter the first number:"))
 y=int(input("enter the second number:"))
-choice=(int(input("enter your choice:1.add\n2.subtract\n3.multiply\n4.divide\n5.exit")))
+choice=(int(input("enter your choice:1.add\n2.subtract\n3.multiply\n4.divide\n5.modulus\n6.floor division\n7.exit\n")))
 if choice==1:
     print(x+y)
 elif choice==2:
@@ -11,5 +11,9 @@ elif choice==3:
      print(x*y)
 elif choice==4:
      print(x/y)
+elif choice==5:
+     print(x%y)
+elif choice==6:
+     print(x//y)        
 else:
    print("invalid choice")    
